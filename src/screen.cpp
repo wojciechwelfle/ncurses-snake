285404f493e8ffc2d8ff0a0edef025dddc7ec475
wojciechwelfle <wojciech.welfle@gmail.com>
2023-12-11 15:38:28 +0100

    add screen files

---
+++ src/screen.cpp
@@ -0,0 +1,34 @@
+/* screen.cpp */
+#include "screen.h"
+
+void init_screen()
+{
+    initscr();
+    cbreak(), noecho();
+    nodelay(stdscr, FALSE);
+    nonl();
+    leaveok(stdscr, TRUE);
+    intrflush(stdscr, FALSE);
+    keypad(stdscr, TRUE);
+    curs_set(0);
+}
+
+void done_screen()
+{
+    endwin();
+}
+
+void gotoyx(int y, int x)
+{
+    wmove(stdscr, y, x);
+}
+
+int ngetch()
+{
+    return wgetch(stdscr);
+}
+
+void getscreensize(int& y, int& x)
+{
+    getmaxyx(stdscr, y, x);
+}
